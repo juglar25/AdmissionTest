0021e95b01fca96ad97cfc924a8e12fdf294f9f6
juan.hilarion <juan.hilarion25@gmail.com>
2023-11-03 14:39:52 -0500

    cambio endpoint2

--- src/main/main.service.ts
+++ src/main/main.service.ts
@@ -2,38 +2,35 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class MainService {
-    private counterId = 1;
-    private sonaAssets: any = [
-        {
-            "profile_name": "IWC Schaffhausen",
-            "profile_image": "https://cdn.xaxiscreativestudios.com/APAC/2023/Test/Prueba_Tecnica_XCS/profile_image.png",
-            "post_description": "Lewis Hamilton introduces the new Big Pilot's Watch 43, an iconic and minimal design for the modern adventurer.",
-            "post_cta": "Discover More",
-            "icons": "https://cdn.xaxiscreativestudios.com/APAC/2023/Test/Prueba_Tecnica_XCS/Icons.svg",
-            "slides": [
+    private sonaAssets: any = {
+            profile_name: "IWC Schaffhausen",
+            profile_image: "https://cdn.xaxiscreativestudios.com/APAC/2023/Test/Prueba_Tecnica_XCS/profile_image.png",
+            post_description: "Lewis Hamilton introduces the new Big Pilot's Watch 43, an iconic and minimal design for the modern adventurer.",
+            post_cta: "Discover More",
+            icons: "https://cdn.xaxiscreativestudios.com/APAC/2023/Test/Prueba_Tecnica_XCS/Icons.svg",
+            slides: [
                 {
-                    "img": "https://cdn.xaxiscreativestudios.com/APAC/2023/Test/Prueba_Tecnica_XCS/slide_1.jpg",
-                    "description": "Engineered for originals"
+                    img: "https://cdn.xaxiscreativestudios.com/APAC/2023/Test/Prueba_Tecnica_XCS/slide_1.jpg",
+                    description: "Engineered for originals"
                 },
                 {
-                    "img": "https://cdn.xaxiscreativestudios.com/APAC/2023/Test/Prueba_Tecnica_XCS/slide_2.jpg",
-                    "description": "Bold and precise"
+                    img: "https://cdn.xaxiscreativestudios.com/APAC/2023/Test/Prueba_Tecnica_XCS/slide_2.jpg",
+                    description: "Bold and precise"
                 },
                 {
-                    "img": "https://cdn.xaxiscreativestudios.com/APAC/2023/Test/Prueba_Tecnica_XCS/slide_3.jpg",
-                    "description": "Made in Switzerland"
+                    img: "https://cdn.xaxiscreativestudios.com/APAC/2023/Test/Prueba_Tecnica_XCS/slide_3.jpg",
+                    description: "Made in Switzerland"
                 },
                 {
-                    "img": "https://cdn.xaxiscreativestudios.com/APAC/2023/Test/Prueba_Tecnica_XCS/slide_1.jpg",
-                    "description": "Engineered for originals"
+                    img: "https://cdn.xaxiscreativestudios.com/APAC/2023/Test/Prueba_Tecnica_XCS/slide_1.jpg",
+                    description: "Engineered for originals"
                 },
                 {
-                    "img": "https://cdn.xaxiscreativestudios.com/APAC/2023/Test/Prueba_Tecnica_XCS/slide_2.jpg",
-                    "description": "Bold and precise"
+                    img: "https://cdn.xaxiscreativestudios.com/APAC/2023/Test/Prueba_Tecnica_XCS/slide_2.jpg",
+                    description: "Bold and precise"
                 },
             ]
-        }
-    ];
+        };
 
     findAll() {
         return this.sonaAssets;
