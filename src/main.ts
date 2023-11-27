6e48e2f49b4d0f2f73240c8c38b484a5acd81821
juan.hilarion <juan.hilarion25@gmail.com>
2023-11-27 17:03:09 -0500

    ajuste cors denuevo

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  app.useGlobalPipes(new ValidationPipe());
-  const port = app.get(ConfigService).get('PORT');
-  await app.listen(port || 3002);
+  await app.listen(3002);
 }
 bootstrap();
