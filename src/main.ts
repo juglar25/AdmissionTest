72c9e7f6af3d147c82b8db3045049116b6402c9b
juan.hilarion <juan.hilarion25@gmail.com>
2023-11-27 16:14:05 -0500

    ajuste cors

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.enableCors();
+  app.useGlobalPipes(new ValidationPipe());
+  const port = app.get(ConfigService).get('PORT');
+  await app.listen(port || 3002);
 }
 bootstrap();
